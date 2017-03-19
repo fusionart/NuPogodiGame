b9a5df78ad13484af140511982b8a4bd2dc23939
Nikolay Iliev <myquattrobg@gmail.com>
2017-03-19 11:56:21 +0200

    Add desktop version to project

---
+++ desktop/src/com/nupogodi/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.nupogodi.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.nupogodi.game.NuPogodiGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new NuPogodiGame(), config);
+	}
+}
