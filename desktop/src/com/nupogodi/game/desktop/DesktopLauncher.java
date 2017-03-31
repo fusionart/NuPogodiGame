e6cb08fdfb7d019259696eede57d0166a842fa5b
DESKTOP-FCAF4EF\USER <kkristinaborisova@gmail.com>
2017-03-31 10:30:06 +0300

    New (main for the users) class "StartScreen.java" that will contain "Play Game" btn , "Settings" btn,  and "Highest score?" btn . This stage will lead to the others by the buttons

--- desktop/src/com/nupogodi/game/desktop/DesktopLauncher.java
+++ desktop/src/com/nupogodi/game/desktop/DesktopLauncher.java
@@ -8,10 +8,18 @@ public class DesktopLauncher {
 	private static final int SCREEN_WIDTH = 600;
 	private static final int SCREEN_HEIGH = 450;
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "Ну Погоди";
-		config.width = SCREEN_WIDTH;
-		config.height = SCREEN_HEIGH;
-		new LwjglApplication(new GameScreen(), config);
+		LwjglApplicationConfiguration config1 = new LwjglApplicationConfiguration();
+		config1.title = "Ну Погоди";
+		config1.width = SCREEN_WIDTH;
+		config1.height = SCREEN_HEIGH;
+		
+		
+		LwjglApplicationConfiguration config2 = new LwjglApplicationConfiguration();
+		config2.title = "Ну Погоди Game Screen";
+		config2.width = SCREEN_WIDTH;
+		config2.height = SCREEN_HEIGH;
+		
+		//new LwjglApplication(new StartScreen(), config1);
+		new LwjglApplication(new GameScreen(), config2);
 	}
 }
---
+++ desktop/src/com/nupogodi/game/desktop/StartScreen.java
@@ -0,0 +1,54 @@
+package com.nupogodi.game.desktop;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.nupogodi.game.Background;
+
+public class StartScreen implements ApplicationListener {
+	static Stage gameHomeStage;
+	
+	@Override
+	public void create() {
+		gameHomeStage = new Stage(new ScreenViewport());
+		Background background = new Background();
+		gameHomeStage.addActor(background);
+
+	}
+
+
+	@Override
+	public void render() {
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		gameHomeStage.draw();
+
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
