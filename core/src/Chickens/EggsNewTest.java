5c69ff63f00b143a351879284aca743ef65a9737
Nikolay Iliev <myquattrobg@gmail.com>
2017-04-03 23:37:27 +0300

    eggs are running from all sides

--- core/src/Chickens/EggsNewTest.java
+++ core/src/Chickens/EggsNewTest.java
@@ -13,16 +13,16 @@ import com.nupogodi.game.GameScreen;
 
 public class EggsNewTest {
 	GameScreen gameScreen = new GameScreen();
-	private static final float MINIMUM_TIME_BETWEEN_EGGS = .7f;
-	private static final float LEFT_START_X = 50;
-	private static final float LEFT_END_X = 350;
-	private static final float RIGHT_START_X = 0;
-	private static final float RIGHT_END_X = 0;
-	private static final float DOWN_START_Y = 350;
+	private static final float MINIMUM_TIME_BETWEEN_EGGS = .5f;
+	private static final float LEFT_START_X = 40;
+	private static final float LEFT_END_X = 150;
+	private static final float RIGHT_START_X = 560;
+	private static final float RIGHT_END_X = 450;
+	private static final float DOWN_START_Y = 150;
 	private static final float DOWN_END_Y = 50;
-	private static final float UP_START_Y = 0;
-	private static final float UP_DOWN_Y = 0;
-	private float speed = 0.5f;
+	private static final float UP_START_Y = 270;
+	private static final float UP_END_Y = 210;
+	private float speed = 0.9f;
 	private float elapsed = 0.01f;
 	private Texture eggTexture;
 	private SingleEgg newEgg;
@@ -40,13 +40,13 @@ public class EggsNewTest {
 			createEggs(LEFT_START_X, LEFT_END_X, DOWN_START_Y, DOWN_END_Y, eggTexture);
 			break;
 		case 2:
-
+			createEggs(LEFT_START_X, LEFT_END_X, UP_START_Y, UP_END_Y, eggTexture);
 			break;
 		case 3:
-
+			createEggs(RIGHT_START_X, RIGHT_END_X, DOWN_START_Y, DOWN_END_Y, eggTexture);
 			break;
 		case 4:
-
+			createEggs(RIGHT_START_X, RIGHT_END_X, UP_START_Y, UP_END_Y, eggTexture);
 			break;
 
 		default:
@@ -67,7 +67,7 @@ public class EggsNewTest {
 	}
 
 	private int generateRandom() {
-		int n = rand.nextInt(1) + 1;
+		int n = rand.nextInt(4) + 1;
 		return n;
 	}
 
@@ -85,18 +85,31 @@ public class EggsNewTest {
 		Iterator<SingleEgg> i = eggList.iterator();
 		while (i.hasNext()) {
 			SingleEgg egg = i.next();
-			moveLeftSide(egg);
-			// if (egg.getX() > 100){
-			// i.remove();
+			moveEggs(egg);
+			if (egg.getStartX() == LEFT_START_X) {
+				if (egg.getEggX() >= egg.getEndX()) {
+					i.remove();
+				}
+			} else {
+				if (egg.getEggX() <= egg.getEndX()) {
+					i.remove();
+				}
+			}
 		}
 		timeSinceLastEgg += Gdx.graphics.getDeltaTime();
 	}
 
-	private void moveLeftSide(SingleEgg egg) {
-		System.out.println(egg.getStartX());
-		System.out.println(egg.getEggX() + " "+egg.directionX(egg.getStartX(), egg.getEndX()) * speed * elapsed);
-		egg.setEggX(egg.getEggX()+egg.directionX(egg.getStartX(), egg.getEndX()) * speed * elapsed);
-		egg.setEggY(egg.getEggY()+egg.directionY(egg.getStartY(), egg.getEndY()) * speed * elapsed);
+	private void moveEggs(SingleEgg egg) {
+		// System.out.println(egg.getStartX());
+		// System.out.println(egg.getEggX() +
+		// egg.directionX(egg.getStartX(), egg.getEndX()) * speed * elapsed);
+		if (egg.getStartX() == LEFT_START_X) {
+			egg.setEggX(egg.getEggX() + egg.directionX(egg.getStartX(), egg.getEndX()) * speed * elapsed);
+			egg.setEggY(egg.getEggY() + egg.directionY(egg.getStartY(), egg.getEndY()) * speed * elapsed);
+		} else {
+			egg.setEggX(egg.getEggX() + egg.directionX(egg.getStartX(), egg.getEndX()) * speed * elapsed);
+			egg.setEggY(egg.getEggY() + egg.directionY(egg.getStartY(), egg.getEndY()) * speed * elapsed);
+		}
 
 	}
 
