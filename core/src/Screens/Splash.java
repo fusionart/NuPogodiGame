426d860aa91ac083cda8e5dec7e351d2b908f953
DESKTOP-FCAF4EF\USER <kkristinaborisova@gmail.com>
2017-04-03 20:57:34 +0300

    adding unimplemented methods to the class Splash

--- core/src/Screens/Splash.java
+++ core/src/Screens/Splash.java
@@ -1,5 +1,58 @@
 package Screens;
 
-public class Splash {
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Splash implements Screen {
+	private SpriteBatch batch;
+	private Sprite splashPic; // for the pic that would be displayed
+
+	public void create() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void render(float delta) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+
+	}
 
 }
