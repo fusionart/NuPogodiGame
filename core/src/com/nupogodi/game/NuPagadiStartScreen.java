f5846b2e4b4494bce2bd59a6c59a03525638f186
DESKTOP-FCAF4EF\USER <kkristinaborisova@gmail.com>
2017-04-03 20:54:26 +0300

    Create new pachage for the screens, adding first screen - Splash

--- core/src/com/nupogodi/game/NuPagadiStartScreen.java
+++ core/src/com/nupogodi/game/NuPagadiStartScreen.java
@@ -1,13 +1,40 @@
 package com.nupogodi.game;
 
+import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 
-public class NuPagadiStartScreen extends Game {
+public class NuPagadiStartScreen extends ApplicationAdapter {
+	private static final String title = "Nu pagadi Game";
 
 	@Override
 	public void create() {
-		// TODO Auto-generated method stub
-		
+		setScreen(new Splash());
+
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		super.resize(300, 600);
+	}
+
+	@Override
+	public void render() {
+		super.render();
+	}
+
+	@Override
+	public void pause() {
+		super.pause();
+	}
+
+	@Override
+	public void resume() {
+		super.resume();
+	}
+
+	@Override
+	public void dispose() {
+		super.dispose();
 	}
 
 }
