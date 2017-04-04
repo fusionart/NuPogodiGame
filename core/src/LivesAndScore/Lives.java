eeb90a1d84310e5e8e4d7bc205fa41949c29c3c4
Nikolay Iliev <myquattrobg@gmail.com>
2017-04-04 20:24:35 +0300

    begin of score and lives implementation

---
+++ core/src/LivesAndScore/Lives.java
@@ -0,0 +1,6 @@
+package LivesAndScore;
+
+public class Lives {
+	private static final int LIVES = 3;
+	int currentLives;
+}
---
+++ core/src/LivesAndScore/Score.java
@@ -0,0 +1,18 @@
+package LivesAndScore;
+
+public class Score {
+	int score;
+
+	public Score(int score) {
+		setScore(score);;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+}
